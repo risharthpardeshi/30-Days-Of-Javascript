720ba9c8ffba0f495f8029cf33708b1d5b98b15a
Risharth Pardeshi <risharthpardeshi@gmail.com>
2023-07-19 10:58:43 +0530

    Add files via upload

---
+++ 01_Day_Introduction/variable.js
@@ -0,0 +1,17 @@
+// Declaring different variables of different data types
+let firstName = 'Rishi' // first name of a person
+let lastName = 'Arya' // last name of a person
+let country = 'India' // country
+let city = 'Indore' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+// Declaring variables with number values
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+let name = 'Rishi', //name of a person
+  job = 'Developer',
+  live = 'Indore'
